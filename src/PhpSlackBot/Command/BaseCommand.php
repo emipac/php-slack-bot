982bae831d6126522d3a9297e7a9620a279a310d
Allan MacGregor <amacgregor@allanmacgregor.com>
2016-09-13 09:04:04 -0400

    Update for better support in the base command

--- src/PhpSlackBot/Command/BaseCommand.php
+++ src/PhpSlackBot/Command/BaseCommand.php
@@ -7,4 +7,23 @@ abstract class BaseCommand extends \PhpSlackBot\Base {
         return $this->execute($message, $context);
     }
 
+    public function getArgs($message) {
+        $args = array();
+        if (isset($message['text'])) {
+            $args = array_values(array_filter(explode(' ', $message['text'])));
+        }
+        $commandName = $this->getName();
+        // Remove args which are before the command name
+        $finalArgs = array();
+        $remove = true;
+        foreach ($args as $arg) {
+            if ($commandName == $arg) {
+                $remove = false;
+            }
+            if (!$remove) {
+                $finalArgs[] = $arg;
+            }
+        }
+        return $finalArgs;
+    }
 }
